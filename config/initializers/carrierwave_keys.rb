1ce0aa91cb07b92b5a25efb6783f205276efd6c9
Cerize Santos <cerizesantos@gmail.com>
2016-03-05 14:43:58 -0800

    Basic welcome page

---
+++ config/initializers/carrierwave_keys.rb
@@ -0,0 +1,7 @@
+ENV["email_user_name"] = "poetxtpoetxt@gmail.com"
+ENV["email_password"] = "hahahaha"
+
+ENV["S3_ACCESS_KEY"] =  "AKIAI5TAEHQBSMV7DCIA"
+ENV["S3_SECRET_KEY"] =  "CMJNsdNI0B9X725a5YveluWDUTFmNH8hfNWPMqhgMo"
+ENV["S3_BUCKET"] =  "fundzy-assets"
+ENV["S3_REGION"] =  "us-west-2"
