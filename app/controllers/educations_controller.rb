ebbe593b61afee400bac70773fe2135aa32a2eaa
Sam Weatherly <weatherlysam@gmail.com>
2016-03-05 09:51:55 -0800

    education controller implemented

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,4 +1,5 @@
 class EducationsController < ApplicationController
+  before_action :authenticate_user
   before_action :find_education, only: [:edit, :update, :destroy]
 
 
@@ -7,13 +8,41 @@ class EducationsController < ApplicationController
   end
 
   def create
-    
+    @education = Education.new education_params
+    #**********needs to take in params[:user_id] if admins can edit user's info*********
+    #@education.user = params.user
+    @education.user = current_user
+    if @education.save
+      redirect_to current_user, notice: "New education successfully added."
+    else
+      render :new, alert: "Education creation failed."
+    end
   end
+
+  def edit
+
+  end
+
+  def update
+    @education.update education_params
+    redirect_to current_user, notice: "Education successfully updated."
+  end
+
+  def destroy
+    @education.destroy
+    redirect_to current_user notice: "Education deleted."
+  end
+
 
   private
 
   def find_education
     @education = Education.find params[:id]
+  end
+
+  def education_params
+    params.require(:education).permit(:start_date, :end_date, :institution, :program,
+                                      :url, :image, :description)
   end
 
 end
