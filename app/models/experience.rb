0ee7c78ff3bbedb0087f75611b9c72fd326c8383
zibs <elizibin@gmail.com>
2016-03-06 15:49:52 -0800

    Mount image uploader

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,8 @@
 class Experience < ActiveRecord::Base
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
+
   validates :title, presence: true
   validates :company, presence: true
   validates :user_id, presence: true
