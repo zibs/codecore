11f023b49332db938e5992cd5d9e012c40832d64
zibs <elizibin@gmail.com>
2016-03-07 08:56:38 -0800

    hotfix seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ end
 
 links_array = Link.all
 
-30.times do
+10.times do
   u = User.create first_name:   Faker::Name.first_name,
                   last_name:    Faker::Name.last_name,
                   email:        Faker::Internet.email,
